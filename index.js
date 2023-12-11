ad5e49b1507e897b318cc2190ac7c9f367310ede
sepideh azari <sepidehazari89@yahoo.com>
2023-12-11 16:41:12 +0100

    1st upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function changePage(){
+    document.getElementById('page1').hidden=true;
+    document.getElementById('page2').hidden=!true
+    document.getElementById('page3').hidden=true;
+    document.getElementById('page4').hidden=true;
+    document.getElementById('page5').hidden=true;
+}
