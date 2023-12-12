cbab3532256f374e046ce90854e733437abe9c8f
sepideh azari <sepidehazari89@yahoo.com>
2023-12-12 16:20:20 +0100

    lastversion

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 
-function changPage(pagex){
-    if(document.getElementById(pagex)=pagex){
-        document
-    }
-}
-function changePage2(){
+document.getElementById('page1').hidden=!true;
+document.getElementById('page2').hidden=true;
+document.getElementById('page3').hidden=true;
+document.getElementById('page4').hidden=true;
+document.getElementById('page42').hidden=true;
+document.getElementById('page43').hidden=true;
+document.getElementById('page44').hidden=true;
+document.getElementById('page5').hidden=true;
+
+function changAvator(){
     document.getElementById('page1').hidden=true;
     document.getElementById('page2').hidden=true;
-    document.getElementById('page3').hidden=!true;
+    document.getElementById('page3').hidden=true;
     document.getElementById('page4').hidden=true;
-    document.getElementById('page4.2').hidden=true;
-    document.getElementById('page4.3').hidden=true;
-    document.getElementById('page4.4').hidden=true;
+    document.getElementById('page42').hidden=true;
+document.getElementById('page43').hidden=true;
+document.getElementById('page44').hidden=true;
     document.getElementById('page5').hidden=true;
 }
 function changePage1(){
@@ -19,9 +23,19 @@ function changePage1(){
     document.getElementById('page2').hidden=true;
     document.getElementById('page3').hidden=true;
     document.getElementById('page4').hidden=!true;
-    document.getElementById('page4.2').hidden=true;
-    document.getElementById('page4.3').hidden=true;
-    document.getElementById('page4.4').hidden=true;
+    document.getElementById('page42').hidden=true;
+    document.getElementById('page43').hidden=true;
+    document.getElementById('page44').hidden=true;
+    document.getElementById('page5').hidden=true;
+}
+function changePage2(){
+    document.getElementById('page1').hidden=true;
+    document.getElementById('page2').hidden=true;
+    document.getElementById('page3').hidden=!true;
+    document.getElementById('page4').hidden=true;
+    document.getElementById('page42').hidden=true;
+    document.getElementById('page43').hidden=true;
+    document.getElementById('page44').hidden=true;
     document.getElementById('page5').hidden=true;
 }
 function changePage3(){
@@ -29,73 +43,70 @@ function changePage3(){
     document.getElementById('page2').hidden=true;
     document.getElementById('page3').hidden=true;
     document.getElementById('page4').hidden=true;
-    document.getElementById('page4.2').hidden=true;
-    document.getElementById('page4.3').hidden=true;
-    document.getElementById('page4.4').hidden=true;
+    document.getElementById('page42').hidden=true;
+    document.getElementById('page43').hidden=true;
+    document.getElementById('page44').hidden=true;
     document.getElementById('page5').hidden=!true;
 }
-function changAvator(){
-        document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=!true;
-        document.getElementById('page2').hidden=true;
-        document.getElementById('page3').hidden=true;
-        document.getElementById('page4').hidden=true;
-        document.getElementById('page4.2').hidden=true;
-    document.getElementById('page4.3').hidden=true;
-    document.getElementById('page4.4').hidden=true;
-        document.getElementById('page5').hidden=true;
-    }
+
     function changePage41(){
         document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=true;
         document.getElementById('page2').hidden=true;
         document.getElementById('page3').hidden=true;
         document.getElementById('page4').hidden=!true;
-        document.getElementById('page4.2').hidden=true;
-        document.getElementById('page4.3').hidden=true;
-        document.getElementById('page4.4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
         document.getElementById('page5').hidden=true;
     }
 
     function changePage42(){
         document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=true;
         document.getElementById('page2').hidden=true;
         document.getElementById('page3').hidden=true;
         document.getElementById('page4').hidden=true;
-        document.getElementById('page4.2').hidden=!true;
-        document.getElementById('page4.3').hidden=true;
-        document.getElementById('page4.4').hidden=true;
+        document.getElementById('page42').hidden=!true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
         document.getElementById('page5').hidden=true;
     }
     function changePage43(){
         document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=true;
         document.getElementById('page2').hidden=true;
         document.getElementById('page3').hidden=true;
         document.getElementById('page4').hidden=true;
-        document.getElementById('page4.2').hidden=true;
-        document.getElementById('page4.3').hidden=!true;
-        document.getElementById('page4.4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=!true;
+        document.getElementById('page44').hidden=true;
         document.getElementById('page5').hidden=true;
     }
     function changePage44(){
         document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=true;
         document.getElementById('page2').hidden=true;
         document.getElementById('page3').hidden=true;
         document.getElementById('page4').hidden=true;
-        document.getElementById('page4.2').hidden=true;
-        document.getElementById('page4.3').hidden=true;
-        document.getElementById('page4.4').hidden=!true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=!true;
         document.getElementById('page5').hidden=true;
     }
+    function goToHome(){
+        document.getElementById('page1').hidden=!true;
+        document.getElementById('page2').hidden=true;
+        document.getElementById('page3').hidden=true;
+        document.getElementById('page4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
+        document.getElementById('page5').hidden=true;
+    }
+    
     function changOpacity(){
         let avs=document.getElementsByClassName("avator");
         console.log(avs.length);
         console.log(avs[2]);
 
-        for(let i=0; i<11; i++){
+        for(let i=0; i<10; i++){
             console.log(i)
             if(i != 2){
                 avs[i].style.opacity=0.5;
@@ -127,16 +138,68 @@ const socket = io("https://boomserver.pick-up.services/", {
       
         socket.emit("prompt", input)
         document.getElementById('page1').hidden=true;
-        document.getElementById('page1.5').hidden=true;
         document.getElementById('page2').hidden=!true;
         document.getElementById('page3').hidden=true;
         document.getElementById('page4').hidden=true;
-        document.getElementById('page4.2').hidden=true;
-        document.getElementById('page4.3').hidden=true;
-        document.getElementById('page4.4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
         document.getElementById('page5').hidden=true;
         
     }
+    socket.on("school", (data)=>{
+        console.log("this is promt from server", data);
+        
+     document.getElementById("Deutsch_Lehrer").textContent=data['Deutsch_Lehrer']
+     document.getElementById("Geographielehrer").textContent=data['Geographielehrer']
+    document.getElementById("Geschichtslehrer").textContent=data['Geschichtslehrer']
+     document.getElementById("Sozialkunde").textContent=data['Sozialkunde']
+    document.getElementById('page1').hidden=true;
+    document.getElementById('page2').hidden=true;
+    document.getElementById('page3').hidden=true;
+    document.getElementById('page4').hidden=!true;
+    document.getElementById('page42').hidden=true;
+    document.getElementById('page43').hidden=true;
+    document.getElementById('page44').hidden=true;
+    document.getElementById('page5').hidden=true;
+    });
 
-    
-    
+    function cat(category){
+       socket.emit("cat", category)
+        document.getElementById('page1').hidden=true;
+        document.getElementById('page2').hidden=!true;
+        document.getElementById('page3').hidden=true;
+        document.getElementById('page4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
+        document.getElementById('page5').hidden=true;
+        
+    }
+    socket.on("results", (data)=>{
+        console.log("this is promt from server", data);
+        
+     document.getElementById("result").textContent=data
+
+    document.getElementById('page1').hidden=true;
+    document.getElementById('page2').hidden=true;
+    document.getElementById('page3').hidden=true;
+    document.getElementById('page4').hidden=true;
+    document.getElementById('page42').hidden=true;
+    document.getElementById('page43').hidden=true;
+    document.getElementById('page44').hidden=true;
+    document.getElementById('page5').hidden=!true;
+    });
+
+    function teachers(teacher){
+       socket.emit("teacher", teacher)
+        document.getElementById('page1').hidden=true;
+        document.getElementById('page2').hidden=!true;
+        document.getElementById('page3').hidden=true;
+        document.getElementById('page4').hidden=true;
+        document.getElementById('page42').hidden=true;
+        document.getElementById('page43').hidden=true;
+        document.getElementById('page44').hidden=true;
+        document.getElementById('page5').hidden=!true;
+        
+    }
